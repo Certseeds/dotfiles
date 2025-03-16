f1c87a8f9b639d0da91d950f7342017efda18eeb
Certseeds <51754303+Certseeds@users.noreply.github.com>
2025-03-16 03:24:57 +0000

    feat: now reg.ps1 using reg.exe
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- powsh/reg.ps1
+++ powsh/reg.ps1
@@ -1,83 +1,94 @@
 $script = {
-    $userPath = "C:\Users\${env:USERNAME}"
+  $userPath = "C:\Users\${env:USERNAME}"
 
-    function HideDesktopIcons(){
-        New-Item `
-          -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
-          -Force
-        Set-ItemProperty `
-          -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
-          -Name "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" `
-          -Value 1
-    }
-    function RemoveImageLibrary(){
-        New-Item `
-          -Path "HKCU:\Software\Classes\CLSID\{E88865EA-0E1C-4E20-9AA6-EDCD0212C87C}" `
-          -Force
-        Set-ItemProperty `
-          -Path "HKCU:\Software\Classes\CLSID\{E88865EA-0E1C-4E20-9AA6-EDCD0212C87C}" `
-          -Name "System.IsPinnedToNameSpaceTree" `
-          -Value 0
-    }
-    function MsEdge(){
-        New-Item `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Force
-        
-        New-Item `
-          -Path "$userPath\scoop\persist\edge" `
-          -ItemType Directory
-        
-        New-Item `
-          -Path "$userPath\scoop\persist\edge\cache" `
-          -ItemType Directory
-        
-        New-Item `
-          -Path "$userPath\scoop\persist\edge\profiles" `
-          -ItemType Directory
-        
-        Set-ItemProperty `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Name "DiskCacheDir" `
-          -Value "$userPath\scoop\persist\edge\cache"
-        
-        Set-ItemProperty `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Name "DnsOverHttpsMode" `
-          -Value "secure"
-        
-        Set-ItemProperty `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Name "DnsOverHttpsTemplates" `
-          -Value "https://doh.pub/dns-query"
-        
-        Set-ItemProperty `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Name "UserDataDir" `
-          -Value "$userPath\scoop\persist\edge\profiles"
-        
-        Set-ItemProperty `
-          -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" `
-          -Name "WebRtcLocalhostIpHandling" `
-          -Value "disable_non_proxied_udp"
-    }
-    function disableNetworkSearch() {
-        New-Item `
-          -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" `
-          -Force
-        Set-ItemProperty `
-          -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" `
-          -Name "DisableSearchBoxSuggestions" `
-          -Value 1
-    }
+  function HideDesktopIcons() {
+    # 创建注册表项
+    reg.exe add `
+      "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
+      /f
+    # 设置属性
+    reg.exe add `
+      "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" `
+      /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" `
+      /t REG_DWORD `
+      /d 1 `
+      /f
+  }
 
-    function main {
-        HideDesktopIcons
-        RemoveImageLibrary
-        MsEdge
-        disableNetworkSearch
-    }
+  function RemoveImageLibrary() {
+    # 创建注册表项
+    reg.exe add `
+      "HKCU\Software\Classes\CLSID\{E88865EA-0E1C-4E20-9AA6-EDCD0212C87C}" `
+      /f
+    
+    # 设置属性
+    reg.exe add `
+      "HKCU\Software\Classes\CLSID\{E88865EA-0E1C-4E20-9AA6-EDCD0212C87C}" `
+      /v "System.IsPinnedToNameSpaceTree" `
+      /t REG_DWORD `
+      /d 0 `
+      /f
+  }
 
-    main
+  function MsEdge() {
+    # 创建注册表项
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /f
+  
+    # 设置属性
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /v "DiskCacheDir" `
+      /t REG_SZ `
+      /d "$userPath\scoop\persist\edge\cache" `
+      /f
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /v "DnsOverHttpsMode" `
+      /t REG_SZ `
+      /d "secure" `
+      /f
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /v "DnsOverHttpsTemplates" `
+      /t REG_SZ `
+      /d "https://doh.pub/dns-query" `
+      /f
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /v "UserDataDir" `
+      /t REG_SZ `
+      /d "$userPath\scoop\persist\edge\profiles" `
+      /f
+    reg.exe add `
+      "HKLM\SOFTWARE\Policies\Microsoft\Edge" `
+      /v "WebRtcLocalhostIpHandling" `
+      /t REG_SZ `
+      /d "disable_non_proxied_udp" `
+      /f
+  }
+  function disableNetworkSearch() {
+    # 创建注册表项
+    reg.exe add `
+      "HKCU\Software\Policies\Microsoft\Windows\Explorer" `
+      /f
+  
+    # 设置属性
+    reg.exe add `
+      "HKCU\Software\Policies\Microsoft\Windows\Explorer" `
+      /v "DisableSearchBoxSuggestions" `
+      /t REG_DWORD `
+      /d 1 `
+      /f
+  }
+  function main {
+    HideDesktopIcons
+    RemoveImageLibrary
+    MsEdge
+    disableNetworkSearch
+  }
+
+  main
 }
 Invoke-Command $script
