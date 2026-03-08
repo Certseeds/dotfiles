87ef9875bf6d93d7c6ead0c013e48c94acca8925
Certseeds <51754303+Certseeds@users.noreply.github.com>
2026-03-08 07:34:40 +0000

    build: update node container
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- container/node/node.start.sh
+++ container/node/node.start.sh
@@ -12,5 +12,9 @@ main() {
         -e https_proxy="" \
         --name "${IMAGE_NAME}" \
         "${SOURCE}"/"${USERNAME}"/"${IMAGE_NAME}"
+    # run pnpm setup
+    # vim the claude code settings.json, to enable allow-dangerous tasks
+    # openclaw main using qwen3.5-plus
+    # cc using glm-5
 }
 main
