134dc86ab9f2fce72b3cb5c8c1232ce481308540
Certseeds <51754303+Certseeds@users.noreply.github.com>
2026-07-01 13:53:23 +0000

    fix: gitignore and path problem
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- container/sshable/ssh.build.sh
+++ container/sshable/ssh.build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euox pipefail
 main() {
-    if [[ ! -f "sshd_config.conf" ]]; then
-        exit 0
-    fi
     local SOURCE="ghcr.io"
     local USERNAME="certseeds"
     local IMAGE_NAME="sshable"
